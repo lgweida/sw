0cc80d0c5a47d4ebada79fef54688ab8e5363c64
Li Gu <ligu2006@gmail.com>
2025-07-30 21:22:39 -0400

    add parse_url.py url_list.py

---
+++ parse_url.py
@@ -0,0 +1,107 @@
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/visits?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/pages-per-visit?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/average-visit-duration?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/bounce-rate?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/page-views?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/bounce-rate?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/page-views?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/total-traffic-and-engagement/visits-split?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&main_domain_only=false&format=json&show_verified=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-and-engagement/visits?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&state=az&main_domain_only=false&format=json&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-and-engagement/pages-per-visit?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&state=az&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-and-engagement/average-visit-duration?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&state=az&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-and-engagement/bounce-rate?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&state=az&main_domain_only=false&format=json&show_verified=false&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-and-engagement/page-views?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&state=az&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/unique-visitors/desktop_unique_visitors?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&state=az&main_domain_only=false&format=json&show_verified=false&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v2/website/amazon.com/mobile-web/visits?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=gb&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v2/website/amazon.com/mobile-web/pages-per-visit?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=gb&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v2/website/amazon.com/mobile-web/average-visit-duration?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=gb&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v2/website/amazon.com/mobile-web/bounce-rate?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=gb&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v2/website/amazon.com/mobile-web/page-views?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=gb&granularity=monthly&main_domain_only=false&format=json&show_verified=false&mtd=false&engaged_only=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/unique-visitors/mobileweb_unique_visitors?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=gb&granularity=monthly&format=json&main_domain_only=false&show_verified=false&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-sources/overview-share?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-sources/engagement-metrics/pages-per-visit?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-sources/engagement-metrics/average-duration?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/traffic-sources/engagement-metrics/bounce-rate?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json&mtd=false' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/amazon.com/ppc-spend/desktop?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&format=json' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v5/website/amazon.com/mobile-traffic-sources/mobile-overview-share?api_key=Add_Your_API_KEY&start_date=2023-01&end_date=2023-03&country=us&granularity=monthly&main_domain_only=false&fomat=json' \
+     --header 'accept: application/json'
+
+curl --request GET \
+     --url 'https://api.similarweb.com/v1/website/cnn.com/ppc-spend/total?api_key=Add_Your_API_KEY&start_date=2024-01&end_date=2024-02&country=us&granularity=monthly' \
+     --header 'accept: application/json'
---
+++ url_list.py
@@ -0,0 +1,405 @@
+[
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "total-traffic-and-engagement/visits",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "total-traffic-and-engagement/pages-per-visit",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "total-traffic-and-engagement/average-visit-duration",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "total-traffic-and-engagement/bounce-rate",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "total-traffic-and-engagement/page-views",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "total-traffic-and-engagement/visits-split",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-and-engagement/visits",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "state": "az",
+            "main_domain_only": "false",
+            "format": "json",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-and-engagement/pages-per-visit",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "state": "az",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-and-engagement/average-visit-duration",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "state": "az",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-and-engagement/bounce-rate",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "state": "az",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-and-engagement/page-views",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "state": "az",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "unique-visitors/desktop_unique_visitors",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "state": "az",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v2",
+        "domain": "amazon.com",
+        "endpoint": "mobile-web/visits",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "gb",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v2",
+        "domain": "amazon.com",
+        "endpoint": "mobile-web/pages-per-visit",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "gb",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v2",
+        "domain": "amazon.com",
+        "endpoint": "mobile-web/average-visit-duration",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "gb",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v2",
+        "domain": "amazon.com",
+        "endpoint": "mobile-web/bounce-rate",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "gb",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v2",
+        "domain": "amazon.com",
+        "endpoint": "mobile-web/page-views",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "gb",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "show_verified": "false",
+            "mtd": "false",
+            "engaged_only": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "unique-visitors/mobileweb_unique_visitors",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "gb",
+            "granularity": "monthly",
+            "format": "json",
+            "main_domain_only": "false",
+            "show_verified": "false",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-sources/overview-share",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-sources/engagement-metrics/pages-per-visit",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-sources/engagement-metrics/average-duration",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "traffic-sources/engagement-metrics/bounce-rate",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json",
+            "mtd": "false"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "amazon.com",
+        "endpoint": "ppc-spend/desktop",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "format": "json"
+        }
+    },
+    {
+        "api_version": "v5",
+        "domain": "amazon.com",
+        "endpoint": "mobile-traffic-sources/mobile-overview-share",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2023-01",
+            "end_date": "2023-03",
+            "country": "us",
+            "granularity": "monthly",
+            "main_domain_only": "false",
+            "fomat": "json"
+        }
+    },
+    {
+        "api_version": "v1",
+        "domain": "cnn.com",
+        "endpoint": "ppc-spend/total",
+        "search_key": "XXX_SEARCH_KEY_XXX",
+        "params": {
+            "start_date": "2024-01",
+            "end_date": "2024-02",
+            "country": "us",
+            "granularity": "monthly"
+        }
+    }
+]
